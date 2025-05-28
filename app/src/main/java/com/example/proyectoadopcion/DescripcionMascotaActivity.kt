38fb09d166065e1f1732b8eaffa866417f7766b5
Christopher Velez <jeremyvelez328@gmail.com>
2025-05-27 22:32:01 -0500

    Descrión terminada

--- app/src/main/java/com/example/proyectoadopcion/DescripcionMascotaActivity.kt
+++ app/src/main/java/com/example/proyectoadopcion/DescripcionMascotaActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.proyectoadopcion.databinding.ActivityDescripcionMascotaBinding
+import com.example.proyectoadopcion.databinding.ActivityUpdateBinding
 
 class DescripcionMascotaActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityDescripcionMascotaBinding
+    private lateinit var db: DBhelper
+    private var mascotaid: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_descripcion_mascota)
+        binding = ActivityDescripcionMascotaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        db = DBhelper(this)
+        mascotaid = intent.getIntExtra("mascota_id", -1)
+        if (mascotaid == -1) {
+            finish()
+            return
+        }
+
+        val mascota = db.getMascotaById(mascotaid)
+        Glide.with(this)
+            .load(mascota.urlImg)
+            .error(R.drawable.cl_linkroto)
+            .into(binding.Descripcionimage)
+        binding.DescripcionTexview.text=mascota.descripcion
+        val contacto = "Celular: 0979026743\nCorreo: coordinador@gmail.com"
+        binding.ContactoTexview.text=contacto
+
     }
 }
